11a2f11ada67673e33837709b5e856186f76f508
zeynepvrl <zeynebvural450@gmail.com>
2024-09-18 20:52:52 +0300

    Regular printing of book information to txt file

--- kitapyurdu/kitapyurdu/spiders/book_spider.py
+++ kitapyurdu/kitapyurdu/spiders/book_spider.py
@@ -6,6 +6,8 @@ import scrapy
 class BooksSpider(scrapy.Spider):
     name = "books"
     page_count=0
+    book_count=0
+    file=open("books.txt" , "a", encoding="UTF-8")        #bu class oluş oluşmaz dosya açılacak, ve en son işlemler bittinde kapanacak. aç kapa yapmak performansı kötü etkileyebilir
 
     start_urls = [                                 #scrapy crawl books komutunu çalıştırdında bu url den bir response döndürece ve sen o response sayfadaki her şeyi scrap crawl edebileceksin
             "https://www.kitapyurdu.com/index.php?route=product/best_sellers&page=1&list_id=1"
@@ -18,11 +20,19 @@ class BooksSpider(scrapy.Spider):
 
         i=0
         while i<len(books_name):
-            yield{
+            """ yield{
                 "name":books_name[i],
                 "author":books_author[i],
                 "publisher":books_publisher[i]
-            }
+            } """
+
+            self.file.write("------------------------------\n")
+            self.file.write(str(self.book_count)+"\n")
+            self.file.write("Kitap ismi:" + books_name[i]+"\n")
+            self.file.write("Yazarı:"+books_author[i]+ "\n")
+            self.file.write("Yayıncı:"+books_publisher[i]+"\n")
+
+            self.book_count+=1
             i+=1
 
         next_url=response.css("a.next::attr(href)").extract_first()
@@ -30,7 +40,7 @@ class BooksSpider(scrapy.Spider):
         if next_url is not None and self.page_count!=5:
             yield scrapy.Request(url=next_url, callback=self.parse)
         else:
-            pass
+            self.file.close()            #yield ile json dosyasına değil de bir file a yazdırdığımız için scrapy crawl book yazmak yeterli, -o book.json eklemeye gerek yok
 
 
 
